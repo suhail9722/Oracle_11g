cc985b85e9c8b7776dd074ef5b4aadbe84d13435
suhail9722 <39465874+suhail9722@users.noreply.github.com>
2024-05-17 23:55:00 -0400

    Update Retrieve_Alternate_Records.sql

--- INTERVIEWQUESTIONS/Retrieve_Alternate_Records.sql
+++ INTERVIEWQUESTIONS/Retrieve_Alternate_Records.sql
@@ -10,5 +10,33 @@ SELECT * ,ROW_NUMBER() OVER (ORDER BY EMPLOYEE_ID) AS ROW_SEQ) AS V
 WHERE MOD(V.ROW_SEQ,2)=1  /*It gives the ODD number)*/
 
 
+APPROCH 2
+
+We can use rownum and MOD function to retrieve the alternate records from a table. 
+To get Even number records: SELECT * FROM (SELECT rownum, ID, Name FROM Employee) WHERE MOD(rownum,2)=0
+To get Odd number records: SELECT * FROM (SELECT rownum, ID, Name FROM Employee) WHERE MOD(rownum,2)=1
+
+APPROCH 3 Using CTE
+
+-- For even-numbered rows
+WITH OrderedEmployees AS (
+    SELECT ROW_NUMBER() OVER (ORDER BY ID) AS RowNum, ID, Name
+    FROM Employee
+)
+SELECT ID, Name
+FROM OrderedEmployees
+WHERE RowNum % 2 = 0;
+
+-- For odd-numbered rows
+WITH OrderedEmployees AS (
+    SELECT ROW_NUMBER() OVER (ORDER BY ID) AS RowNum, ID, Name
+    FROM Employee
+)
+SELECT ID, Name
+FROM OrderedEmployees
+WHERE RowNum % 2 = 1;
+
+
+
 
 
